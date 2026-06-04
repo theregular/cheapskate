14899a512ebd9f7d5a7435d1ebb7af105681af50
Ben Kartchner <34661616+theregular@users.noreply.github.com>
2026-06-04 00:23:32 -0600

    rn reusables setup

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,11 +1,12 @@
-const path = require('path');
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const path = require("path");
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, {
-  input: path.join(__dirname, 'src/global.css'),
-  configPath: path.join(__dirname, 'tailwind.config.js'),
+  input: path.join(__dirname, "src/global.css"),
+  configPath: path.join(__dirname, "tailwind.config.js"),
+  inlineRem: 16,
   disableTypeScriptGeneration: true,
 });
--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,17 +1,22 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { ThemeProvider } from '@react-navigation/native';
+import { PortalHost } from '@rn-primitives/portal';
 import React from 'react';
 import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
 import '@/global.css';
+import { NAV_THEME } from '@/lib/theme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const systemColorScheme = useColorScheme();
+  const colorScheme = systemColorScheme === 'dark' ? 'dark' : 'light';
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={NAV_THEME[colorScheme]}>
       <AnimatedSplashOverlay />
       <AppTabs />
+      <PortalHost />
     </ThemeProvider>
   );
 }
